1e5e4ab0043b7c3c289b6915522edb858cb6905c
Mohamed Mokhtar <mohamed.hussein@ezdiharfintech.com>
2023-08-13 14:14:44 +0300

    correct getProducts endpoint with proper name

--- lib/core/network/api_strings.dart
+++ lib/core/network/api_strings.dart
@@ -1,4 +1,4 @@
 class ApiStrings {
   static const String baseUrl = 'https://fakestoreapi.com/';
-  static const String getProducts = 'productkkk';
+  static const String getProducts = 'products';
 }
